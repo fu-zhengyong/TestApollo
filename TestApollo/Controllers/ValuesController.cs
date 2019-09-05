e1f2d642c2d660397fb2ea47866713c48d78031a
fuzy <fxy6781349@qq.com>
2019-09-05 11:34:50 +0800

    first commit

---
+++ TestApollo/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace TestApollo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            string title = _configuration.GetValue<string>("service.title");
+
+            return title+",数据库链接："+_configuration["DB.Connection"];
+        }
+    }
+}
